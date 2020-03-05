6bc70673b5a6be06ec57dbf41b4fb62b39e2d108
s18621 <s18621@pjwstk.edu.pl>
2020-03-05 13:08:53 +0100

    Commit Mod

--- Cw1/Cw1/Program.cs
+++ Cw1/Cw1/Program.cs
@@ -1,11 +1,34 @@
 ﻿using System;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace Cw1
 {
-    class Program
+    public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
+            
+            var client = new HttpClient();
+            var result = await client.GetAsync("http://www.pja.edu.pl");
+            //Task<HttpResponseMessage>
+            //ThreadPool()
+
+            if (!result.IsSuccessStatusCode) return;
+
+            string html = await result.Content.ReadAsStringAsync();
+            var regex = new Regex("[a-z]+[a-z0-9]*@[a-z.]+",RegexOptions.IgnoreCase);
+            var matches = regex.Matches(html);
+
+            foreach(var m in matches)
+            {
+                Console.WriteLine(m);
+             
+            }
+
+
             Console.WriteLine("Hello World!");
         }
     }
